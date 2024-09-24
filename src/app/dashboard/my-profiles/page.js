27418292890db29442502aa83d729b5c3d8a64d8
danial momen <danialmomenpour@gmail.com>
2024-09-24 14:35:34 +0330

    fixed bug-5

--- src/app/dashboard/my-profiles/page.js
+++ src/app/dashboard/my-profiles/page.js
@@ -6,7 +6,6 @@ import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import Client from "@/models/Client";
 
 import MyProfilesPage from "@/templates/MyProfilesPage";
-import { redirect } from "next/navigation";
 
 async function MyProfiles() {
   await connectDB();
@@ -14,9 +13,6 @@ async function MyProfiles() {
   const session = await getServerSession(authOptions);
   //   console.log(session);
 
-  if (session) {
-    redirect("/dashboard/my-profiles");
-  }
 
   const [client] = await Client.aggregate([
     { $match: { email: session.user.email } },
