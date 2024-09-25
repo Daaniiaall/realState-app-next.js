42d559afaa7113581a820fc888ca85c21ba512f9
danial momen <danialmomenpour@gmail.com>
2024-09-25 10:47:21 +0330

    edit dashboard page

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,9 +1,19 @@
-import DashboardPage from '@/templates/DashboardPage'
+import Client from "@/models/Client";
+import connectDB from "@/utils/auth/connectDB";
 
-function Dashboard() {
-  return (
-    <DashboardPage />
-  )
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+
+import DashboardPage from "@/templates/DashboardPage";
+
+async function Dashboard() {
+  await connectDB();
+  const session = await getServerSession(authOptions);
+  const client = await Client.findOne({ email: session.user.email });
+  // console.log(client.createdAt)
+  const createdAt = client.createdAt;
+
+  return <DashboardPage createdAt={createdAt} />;
 }
 
-export default Dashboard
+export default Dashboard;
--- src/components/templates/DashboardPage.js
+++ src/components/templates/DashboardPage.js
@@ -1,25 +1,13 @@
 import styles from "@/templates/DashboardPage.module.css";
 
-import Client from "@/models/Client";
-import connectDB from "@/utils/auth/connectDB";
-
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-
-async function DashboardPage() {
-  await connectDB();
-  const session = await getServerSession(authOptions);
-  const client = await Client.findOne({ email: session.user.email });
-  // console.log(client.createdAt)
-  const date = client.createdAt.toLocaleDateString()
-  
+function DashboardPage({ createdAt }) {
   return (
     <div className={styles.container}>
       <h3>سلام👋</h3>
       <p>آگهی خود را ثبت کنید تا هزاران نفر آن را مشاهده کنند</p>
       <div className={styles.createdAt}>
         <span>تاریخ عضویت:</span>
-        <p>{date}</p>
+        <p>{new Date(createdAt).toLocaleDateString("fa-IR")}</p>
       </div>
     </div>
   );
